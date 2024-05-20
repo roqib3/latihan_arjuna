4f15992d9d855b51ffb14fdde79e5d5edc42227f
Ntune0241 <129527507+Ntune0241@users.noreply.github.com>
2024-05-20 22:47:47 +0700

    route berhasil
    experiment

---
+++ src/MasukPage/Masuk.jsx
@@ -0,0 +1,12 @@
+import { NavbarGuest } from "../NavbarGuest/NavbarGuest";
+
+const Masuk = () =>{
+    return(
+        <> 
+        <NavbarGuest />
+        <h1>Masuk</h1>
+        </>
+    );
+};
+
+export default Masuk
