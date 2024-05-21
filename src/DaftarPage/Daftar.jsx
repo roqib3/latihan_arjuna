65d8f36f859d9441e324721d2ad37874364efaa0
Ntune0241 <129527507+Ntune0241@users.noreply.github.com>
2024-05-21 21:36:07 +0700

    sign in modif

---
+++ src/DaftarPage/Daftar.jsx
@@ -0,0 +1,12 @@
+import { NavbarGuest } from "../NavbarGuest/NavbarGuest"
+
+const Daftar = () =>{
+    return(
+        <>
+        <NavbarGuest />
+        <h1>haiyooo</h1>
+        </>
+    )
+}
+
+export default Daftar
