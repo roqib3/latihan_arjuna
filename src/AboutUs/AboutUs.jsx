dec051d3796d3ff402e8e6c8bf7bb81c6b576bda
izzulmaula <izzulmaula91@gmail.com>
2024-05-21 22:40:05 +0700

    test

---
+++ src/AboutUs/AboutUs.jsx
@@ -0,0 +1,97 @@
+import { Button2Property1Default } from "../Button2Property1Default/Button2Property1Default.jsx";
+import { ContainerBerita } from "../ContainerBerita/ContainerBerita.jsx";
+import { Footer } from "../Footer/Footer.jsx";
+import { SubJudulFix } from "../SubJudulFix/SubJudulFix.jsx";
+import { NavbarGuest } from "../NavbarGuest/NavbarGuest.jsx";
+
+export const AboutUs = ({ className, ...props }) => {
+    return (
+        
+    <div className="w-[1440px] h-[1066px] relative bg-white">
+    <div className="w-[1674px] h-[1674px] left-[324px] top-[363px] absolute opacity-40 bg-green-800 rounded-full blur-[550px]" />
+    <div className="w-[682px] h-[683px] left-[965px] top-[-192px] absolute opacity-40 bg-emerald-200 rounded-full blur-[500px]" />
+    <div className="w-[690px] left-[122px] top-[254px] absolute"><span style="text-black text-base font-normal font-['Nunito'] leading-snug">Reliji Trash Care adalah website pengelolaan sampah yang menjadi solusi bagi pengelolaan sampah di lingkungan Desa Mendalo Indah, Jambi, yang dikelola oleh jasa pengangkut sampah Reliji.<br/></span><span style="text-black text-base font-normal font-['Nunito'] leading-snug"><br/>“Reliji” merupakan jasa angkutan sampah bersifat swadaya masyarakat yang bekerja sama dengan Dinas Lingkungan Hidup. Reliji menjadi Tempat Pembuangan Sampah Sementara (TPS) bagi warga Desa Mendalo Indah. Sampah yang terkumpul kemudian akan diangkut oleh pihak Dinas Lingkungan Hidup ke Tempat Pembuangan Akhir (TPA) yang bertempat di Bukit Baling, Sengeti.<br/><br/>Dengan adanya website ini, kami berharap masyarakat Desa Mendalo Indah dapat lebih mudah dalam mengakses informasi pengelolaan sampah yang dilakukan oleh pihak Reliji.</span></div>
+    <div className="w-[1208px] h-[0px] left-[120px] top-[1272px] absolute border border-white"></div>
+    <div className="w-[1440px] h-[1024px] left-[289px] top-[-229px] absolute">
+        <div className="w-[1440px] h-[1024px] left-0 top-0 absolute bg-gradient-to-l from-black to-neutral-500" />
+        <img className="w-[1440px] h-[1024px] left-[-81px] top-[83px] absolute" src="https://via.placeholder.com/1440x1024" />
+        <div className="w-[1246px] h-[1246px] left-[639px] top-[480px] absolute opacity-70 bg-green-800 rounded-full blur-[550px]" />
+        <div className="w-[508px] h-[508px] left-[1116px] top-[67px] absolute opacity-70 bg-emerald-200 rounded-full blur-[500px]" />
+    </div>
+    <div className="left-0 top-[710px] absolute flex-col justify-start items-start gap-2.5 inline-flex">
+        <div className="w-[1440px] h-[356px] relative">
+            <div className="w-[1440px] h-[356px] left-0 top-0 absolute bg-green-800" />
+            <div className="w-[189px] h-[42px] p-2.5 left-[626px] top-[252px] absolute justify-center items-center gap-2.5 inline-flex">
+                <div className="text-white text-base font-normal font-['Nunito'] leading-snug">Copyright © 2024 Reliji</div>
+            </div>
+            <div className="w-[674px] h-40 left-[120px] top-[42px] absolute flex-col justify-start items-start gap-5 inline-flex">
+                <div className="w-[272px] h-[54px] text-white text-[32px] font-semibold font-['Nunito'] leading-[48px] tracking-tight">Kontak kami</div>
+                <div className="flex-col justify-start items-start gap-[7px] flex">
+                    <div className="justify-start items-center gap-2 inline-flex">
+                        <img className="w-6 h-6" src="https://via.placeholder.com/24x24" />
+                        <div className="text-white text-base font-normal font-['Nunito'] leading-snug">Desa Mendalo Indah, Kecamatan Jambi Luar Kota, Kabupaten Muaro Jambi, Provinsi Jambi</div>
+                    </div>
+                    <div className="justify-start items-start gap-2 inline-flex">
+                        <img className="w-6 h-6" src="https://via.placeholder.com/24x24" />
+                        <div className="text-white text-base font-normal font-['Nunito'] leading-snug">081234567890</div>
+                    </div>
+                    <div className="justify-start items-start gap-2 inline-flex">
+                        <img className="w-6 h-6" src="https://via.placeholder.com/24x24" />
+                        <div className="text-white text-base font-normal font-['Nunito'] leading-snug">relijitrashcare@gmail.com</div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-[1208px] h-[0px] left-[120px] top-[248px] absolute border border-white"></div>
+            <div className="w-[354px] h-60 left-[543px] top-[58px] absolute rounded-[10px]" />
+        </div>
+    </div>
+    <div className="left-0 top-[2px] absolute flex-col justify-center items-center inline-flex">
+        <div className="justify-start items-center inline-flex">
+            <div className="w-[25.43px] h-[25.37px] relative">
+            </div>
+            <div><span style="text-green-800 text-xl font-bold font-['Nunito']">eliji </span><span style="text-green-800 text-xl font-light font-['Nunito']">Trash Care</span></div>
+        </div>
+    </div>
+    <div className="left-[122px] top-[194px] absolute flex-col justify-center items-center inline-flex">
+        <div className="justify-start items-center inline-flex">
+            <div className="w-[25.43px] h-[25.37px] relative">
+            </div>
+            <div><span style="text-green-800 text-xl font-bold font-['Nunito']">eliji </span><span style="text-green-800 text-xl font-light font-['Nunito']">Trash Care</span></div>
+        </div>
+    </div>
+    <div className="w-[1440px] px-[120px] py-2.5 left-0 top-0 absolute bg-white rounded-bl-[15px] rounded-br-[15px] shadow backdrop-blur-[25px] justify-between items-start inline-flex">
+        <div className="justify-start items-center gap-6 flex">
+            <div className="flex-col justify-center items-center inline-flex">
+                <div className="justify-start items-center inline-flex">
+                    <div className="w-[25.43px] h-[25.37px] relative">
+                    </div>
+                    <div><span style="text-green-800 text-xl font-bold font-['Nunito']">eliji </span><span style="text-green-800 text-xl font-light font-['Nunito']">Trash Care</span></div>
+                </div>
+            </div>
+            <div className="justify-start items-center gap-[5px] flex">
+                <div className="px-4 py-2.5 justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-green-800 text-base font-normal font-['Nunito'] leading-snug">Beranda</div>
+                </div>
+                <div className="px-4 py-2.5 justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-green-800 text-base font-bold font-['Nunito'] underline leading-snug">Tentang Kami</div>
+                </div>
+                <div className="px-4 py-2.5 justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-green-800 text-base font-normal font-['Nunito'] leading-snug">Layanan</div>
+                </div>
+                <div className="px-4 py-2.5 justify-center items-center gap-2.5 flex">
+                    <div className="text-center text-green-800 text-base font-normal font-['Nunito'] leading-snug">Berita</div>
+                </div>
+            </div>
+        </div>
+        <div className="justify-start items-start gap-2.5 flex">
+            <div className="px-4 py-2.5 bg-white rounded-[10px] shadow justify-center items-center gap-2.5 flex">
+                <div className="text-center text-green-800 text-sm font-bold font-['Nunito'] leading-tight">Masuk</div>
+            </div>
+            <div className="px-4 py-2.5 bg-green-800 rounded-[10px] shadow justify-center items-center gap-2.5 flex">
+                <div className="text-center text-white text-sm font-bold font-['Nunito'] leading-tight">Daftar</div>
+            </div>
+        </div>
+    </div>
+</div>
+);
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 import { LandingPage } from "./LandingPage/LandingPage";
+import { AboutUs } from "./AboutUs/AboutUs";
 
 export default function App() {
   return (
